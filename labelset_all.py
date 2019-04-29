bb7814ee9fb113ad08153f06b9dd1793c8002865
Manuel Mendoza <manuelmendozahurtado@hotmail.com>
2019-04-29 14:44:14 +0200

    script for executing all the folds at once

---
+++ labelset_all.py
@@ -0,0 +1,20 @@
+#Listado de datasets a ejecutar
+dataset = {
+   'delicious',
+   'bookmarks',
+   'mediamill',
+   'tmc2007',
+   'bibtex',
+   'corel5k',
+   'emotions',
+   'enron',
+   'genbase',
+   'medical',
+   'scene',
+   'yeast'
+}
+
+for s in dataset:
+   name='./datasets/'+s+'/'+s'.arff'
+   #run python2.7 kfold-labelset.py './datasets/'+s+'/'+s'.arff'
+   os.system('nohup python2.7 /home/i32petoj/pruebasManuel/multilabel/multilabel-labelset.py ' + str(name) + ' ' + str(10) + ' &')
